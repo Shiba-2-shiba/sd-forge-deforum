4067747356b1571330c5bd29db57f63a3cce2c70
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-06-06 15:26:19 +0900

    Update hunyuan_video_packed.py

--- scripts/framepack/hunyuan_video_packed.py
+++ scripts/framepack/hunyuan_video_packed.py
@@ -5,15 +5,15 @@ import einops
 import torch.nn as nn
 import numpy as np
 
-from diffusers.loaders import FromOriginalModelMixin
-from diffusers.configuration_utils import ConfigMixin, register_to_config
-from diffusers.loaders import PeftAdapterMixin
-from diffusers.utils import logging
-from diffusers.models.attention import FeedForward
-from diffusers.models.attention_processor import Attention
-from diffusers.models.embeddings import TimestepEmbedding, Timesteps, PixArtAlphaTextProjection
-from diffusers.models.modeling_outputs import Transformer2DModelOutput
-from diffusers.models.modeling_utils import ModelMixin
+from ..diffusers.loaders import FromOriginalModelMixin
+from ..diffusers.configuration_utils import ConfigMixin, register_to_config
+from ..diffusers.loaders import PeftAdapterMixin
+from ..diffusers.utils import logging
+from ..diffusers.models.attention import FeedForward
+from ..diffusers.models.attention_processor import Attention
+from ..diffusers.models.embeddings import TimestepEmbedding, Timesteps, PixArtAlphaTextProjection
+from ..diffusers.models.modeling_outputs import Transformer2DModelOutput
+from ..diffusers.models.modeling_utils import ModelMixin
 from .dit_common import LayerNorm
 from .utils import zero_module
 
