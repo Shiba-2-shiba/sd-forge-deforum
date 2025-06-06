0eb56e407d42da257852ed271d0b2bca89bc8da5
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-06-06 12:18:26 +0900

    Update k_diffusion_hunyuan.py

--- scripts/framepack/k_diffusion_hunyuan.py
+++ scripts/framepack/k_diffusion_hunyuan.py
@@ -3,7 +3,7 @@ import math
 
 from diffusers_helper.k_diffusion.uni_pc_fm import sample_unipc
 from diffusers_helper.k_diffusion.wrapper import fm_wrapper
-from diffusers_helper.utils import repeat_to_batch_size
+from .utils import repeat_to_batch_size
 
 
 def flux_time_shift(t, mu=1.15, sigma=1.0):
