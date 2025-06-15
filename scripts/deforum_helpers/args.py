d3b01d925ed32fac8ce1a254116decb943a4fe54
Shiba*2 <135475967+Shiba-2-shiba@users.noreply.github.com>
2025-06-16 02:05:33 +0900

    Update args.py

--- scripts/deforum_helpers/args.py
+++ scripts/deforum_helpers/args.py
@@ -1477,10 +1477,10 @@ def FramePackF1Args():
         "f1_image_strength": {
             "label": "Image Strength (F1 Mode)",
             "type": "slider",
-            "minimum": 0.0,
+            "minimum": 0.99,
             "maximum": 1.0,
-            "step": 0.01,
-            "value": 0.85,
+            "step": 0.0001,
+            "value": 1.8,
             "info": "Influence of the initial image. Higher values stick closer to the start image. (1.0 = 100%)",
         },
         "f1_generation_latent_size": {
